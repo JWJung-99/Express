d3bee3ee57ee6b21f6ae483aeabff816401b4321
JWJung-99 <39busy@naver.com>
2025-06-20 00:07:52 +0900

    Chapter09: module 형식으로 변경

--- server/db/database.js
+++ server/db/database.js
@@ -1,5 +1,5 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+import mysql from 'mysql2/promise';
+import 'dotenv/config';
 
 // Create the connection pool. The pool-specific settings are the defaults
 const pool = mysql.createPool({
