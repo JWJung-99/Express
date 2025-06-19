9b12168a4e518b726fe8893e9a93263fac31d8cb
JWJung-99 <39busy@naver.com>
2025-06-19 21:07:14 +0900

    Chapter09: MySQL+Express

---
+++ server/db/database.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+// Create the connection pool. The pool-specific settings are the defaults
+const pool = mysql.createPool({
+	host: process.env.MYSQL_RDS_ENDPOINT,
+	user: process.env.MYSQL_RDS_USERNAME,
+	password: process.env.MYSQL_RDS_PASSWORD,
+	port: 3306,
+	database: 'db_test',
+});
