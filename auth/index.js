84a56199f112150332594beece9d4dc13f7a0b61
JWJung-99 <39busy@naver.com>
2025-06-26 20:56:30 +0900

    Chapter15: HTTP Authentication

--- auth/index.js
+++ auth/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
 
 const app = express();
 const port = 3000;
@@ -50,6 +52,33 @@ function authorization(req, res, next) {
 	next();
 }
 
+function authorizationJWT(req, res, next) {
+	const auth = req.headers.authorization;
+
+	if (!auth) {
+		res.sendStatus(401);
+		return;
+	}
+
+	const value = auth.split(' ')[1];
+
+	// Bearer 처리
+	const decoded = jwt.verify(value, 'secret');
+	const user = users[0];
+
+	if (decoded.id !== user.id) {
+		res.sendStatus(401);
+		return;
+	}
+
+	if (decoded.name !== user.name) {
+		res.sendStatus(401);
+		return;
+	}
+
+	next();
+}
+
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
@@ -58,8 +87,43 @@ app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
 
-app.get('/notes', authorization, (req, res) => {
+app.post('/token', (req, res, next) => {
+	try {
+		const { id, password } = req.body;
+		const user = users[0];
+
+		if (id !== user.id) {
+			const error = new Error('존재하지 않는 아이디입니다.');
+			error.status = 401;
+			throw error;
+		}
+
+		if (password !== user.password) {
+			const error = new Error('비밀번호를 확인해주세요.');
+			error.status = 401;
+			throw error;
+		}
+
+		const token = jwt.sign({ id, name: user.name }, 'secret', {
+			expiresIn: '1h',
+		});
+
+		res.send({ token });
+	} catch (err) {
+		next(err);
+	}
+});
+
+app.get('/notes', authorizationJWT, (req, res) => {
 	const note = notes[0];
 
 	res.send(note);
+});
+
+// Error Handling
+app.use((err, req, res, next) => {
+	const status = err.status || 500;
+	const message = err.message || 'Internal Server Error';
+
+	res.status(status).send(message);
 });
